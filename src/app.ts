884aac0055dd344fac6f54aad87c02596a8b76ff
chakravarthichowdary <chakravarthichowdary236@gmail.com>
2022-05-30 09:35:01 +0530

    Mongo db url changed

--- src/app.ts
+++ src/app.ts
@@ -45,15 +45,12 @@ app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
 });
 
 mongoose
-  .connect(
-    `mongodb+srv://chakri:chakri@cluster0.pkkrb.mongodb.net/expensetracker?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
+  .connect(`your mongodb url`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => {
     app.listen(port, () => {
       console.log(`Server started at port ${port}`);
